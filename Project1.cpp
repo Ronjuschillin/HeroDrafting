cde78289a14c6188c46f0a23579cda1407afd553
Yhan-ux <moraoryan2@gmail.com>
2025-05-07 18:49:32 +0800

    add Hero.h

--- Project1.cpp
+++ Project1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "gari kasta si wahh!\n";
     std::cout << " TRY ";
+    std::cout << "testing";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
